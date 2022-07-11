037f7f72a37077fe8288107f9c41767fe0d034a8
Vinícius Brunheroto <75040937+ViniciusBrunheroto@users.noreply.github.com>
2022-07-11 16:46:24 -0300

    Update FuncionarioServiceImpl.java

--- projeto/src/main/java/br/unesp/projeto/service/FuncionarioServiceImpl.java
+++ projeto/src/main/java/br/unesp/projeto/service/FuncionarioServiceImpl.java
@@ -7,19 +7,51 @@ package br.unesp.projeto.service;
 import br.unesp.projeto.dao.FuncionarioDAO;
 import br.unesp.projeto.dao.DaoFactory;
 import br.unesp.projeto.model.Funcionario;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class FuncionarioServiceImpl implements FuncionarioService {
-    private FuncionarioDAO dao = DaoFactory.getFuncionarioDAO();
 
+  private FuncionarioDAO dao = DaoFactory.getFuncionarioDAO();
+  
     @Override
     public boolean save(Funcionario entity) {
         boolean b = false;
 
         if (dao != null) {
+            System.out.println("NOT NULL");
             dao.save(entity);
             b = true;
         }
 
         return b;
     }
-}
+
+    @Override
+    public Funcionario findById(Long id) {
+        Funcionario entity = null;
+
+        if (dao != null) {
+            entity = dao.findById(id);
+        }
+
+        return entity;
+    }
+
+
+    @Override
+    public List<Funcionario> findAll() {
+        List<Funcionario> lista = null;
+
+        if (dao != null) {
+            lista = new ArrayList<>();
+            lista = dao.findAll();
+        }
+
+        return lista;
+    }
+
+}  
+  
+  
