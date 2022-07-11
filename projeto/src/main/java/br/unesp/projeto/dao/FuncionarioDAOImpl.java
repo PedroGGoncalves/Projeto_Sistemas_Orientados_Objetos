d3ed9cd8a75b815c590030c155c76fa3aa3347b4
Vinícius Brunheroto <75040937+ViniciusBrunheroto@users.noreply.github.com>
2022-07-11 16:48:11 -0300

    Update FuncionarioDAOImpl.java

--- projeto/src/main/java/br/unesp/projeto/dao/FuncionarioDAOImpl.java
+++ projeto/src/main/java/br/unesp/projeto/dao/FuncionarioDAOImpl.java
@@ -5,6 +5,7 @@
  */
 package br.unesp.projeto.dao;
 import br.unesp.projeto.model.Funcionario;
+import br.unesp.projeto.model.Login;
 import br.unesp.projeto.utils.FabricaConexao;
 
 import java.sql.Connection;
@@ -12,6 +13,8 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FuncionarioDAOImpl implements FuncionarioDAO {
     @Override
@@ -45,5 +48,79 @@ public class FuncionarioDAOImpl implements FuncionarioDAO {
         }
 
         return b;
+    }
+    
+    
+    @Override
+    public Funcionario findById(Long idFuncionario) {
+
+        Connection con = null;
+        PreparedStatement pstm = null;
+        ResultSet res = null;
+        Funcionario func = null;
+
+        con = FabricaConexao.getConexao();
+
+        if (con != null) {
+            try {
+                pstm = con.prepareStatement(FIND_BY_ID);
+                pstm.setLong(1, idFuncionario);
+                res = pstm.executeQuery();
+
+                while (res.next()) {
+                    func = new Funcionario();
+                    func.setIdFuncionario(res.getLong(1));
+                    func.setNome(res.getString(2));
+                    func.setRG(res.getString(3));
+                    func.setCPF(res.getString(4));
+                    func.setsalario(res.getFloat(5));
+                    func.setdata_nascimento(res.getDate(6));
+                    func.setcontratacao(res.getDate(7));
+                    func.setlogin(res.getBoolean(8));
+                    func.setLoginn((List<Login>) res.getArray(9));
+                }
+            } catch (SQLException ex) {
+                System.out.println("Message: " + ex);
+            }
+        }
+
+        return func;
+    }
+
+ @Override
+    public List<Funcionario> findAll() {
+
+        Connection con = null;
+        PreparedStatement pstm = null;
+        ResultSet res = null;
+        List<Funcionario> lista = new ArrayList<>();
+
+        con = FabricaConexao.getConexao();
+
+        if (con != null) {
+            try {
+                pstm = con.prepareStatement(FIND_ALL);                
+                res = pstm.executeQuery();
+                                
+                while (res.next()) {                    
+                    Funcionario func = new Funcionario();
+                    func.setIdFuncionario(res.getLong(1));
+                    func.setNome(res.getString(2));
+                    func.setRG(res.getString(3));
+                    func.setCPF(res.getString(4));
+                    func.setsalario(res.getFloat(5));
+                    func.setdata_nascimento(res.getDate(6));
+                    func.setcontratacao(res.getDate(7));
+                    func.setlogin(res.getBoolean(8));
+                    func.setLoginn((List<Login>) res.getArray(9));
+                    
+                    lista.add(func);
+                }
+            } catch (SQLException ex) {
+                System.out.println("Message: " + ex);
+            }
+        }
+
+        return lista;
     }
 }
