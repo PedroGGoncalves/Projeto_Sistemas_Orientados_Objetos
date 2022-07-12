445e60c6f84358391cd681768ff45ad9c148e1a0
Vinícius Brunheroto <75040937+ViniciusBrunheroto@users.noreply.github.com>
2022-07-12 11:14:29 -0300

    Update Aquario.java

--- projeto/src/main/java/br/unesp/projeto/model/Aquario.java
+++ projeto/src/main/java/br/unesp/projeto/model/Aquario.java
@@ -11,6 +11,8 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import org.springframework.data.annotation.Id;
 
 
@@ -21,6 +23,7 @@ import org.springframework.data.annotation.Id;
 
 public class Aquario {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long idAquario;
     private String nome;
     private String endereco;
@@ -32,81 +35,5 @@ public class Aquario {
 
     }
     
-    public long getIdAquario() {
-        return idAquario;
-    }
-
-    public void setIdAquario(long idAquario) {
-        this.idAquario = idAquario;
-    }
-    
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public String getendereco() {
-        return endereco;
-    }
-
-    public void setendereco(String endereco) {
-        this.endereco = endereco;
-    }
-    
-    public String gethorario_func() {
-        return horario_func;
-    }
-
-    public void sethorario_func(String horario_func) {
-        this.horario_func = horario_func;
-    }
-    
-     public String getcontato() {
-        return contato;
-    }
-
-    public void setcontato(String contato) {
-        this.contato = contato;
-    }
-    
-    public float getprecoIngresso() {
-        return precoIngresso;
-    }
-
-    public void setprecoIngresso(float precoIngresso) {
-        this.precoIngresso = precoIngresso;
-    }
-    
-    @Override
-    public int hashCode() {
-        int hash = 5;
-        hash = 37 * hash + (int) (this.idAquario ^ (this.idAquario >>> 32));
-        return hash;
-    }
-    
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Aquario other = (Aquario) obj;
-        if (this.idAquario!= other.idAquario) {
-            return false;
-        }
-        return true;
-    }
-    @Override
-    public String toString() {
-        return "Aquario{" + "idAquario=" + idAquario + ", nome=" + nome
-                + ", endereco=" + endereco + ", horario_func=" + horario_func + ", contato=" + contato +
-                ", precoIngresso=" + precoIngresso + '}';
-    }
+  
 }
