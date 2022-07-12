3c689fe44cd7cad820eab97b5ca8ea645c663030
Pedro Gabriel Gonçalves <85896745+PedroGGoncalves@users.noreply.github.com>
2022-07-12 20:51:49 -0300

    Update medidor_OxigenaçãoDAO.java

--- projeto/src/main/java/br/unesp/projeto/dao/medidor_OxigenaçãoDAO.java
+++ projeto/src/main/java/br/unesp/projeto/dao/medidor_OxigenaçãoDAO.java
@@ -5,10 +5,19 @@
  */
 package br.unesp.projeto.dao;
 import br.unesp.projeto.model.Medidor_oxigenacao;
+import java.util.List;
 
 public interface medidor_OxigenaçãoDAO {
      final String INSERT_Medidor_oxigenacao = "INSERT INTO medidor_oxigenacao(oxigenacao) "
             + "VALUES(?)";
+     final String FIND_BY_ID = "SELECT idMedidor_oxigenacao, oxigenacao"
+            + "FROM medidor_oxigenacao WHERE id_Medidor_oxigenacao = ?";
+
+    final String FIND_ALL = "SELECT idMedidor_oxigenacao, oxigenacao"
+            + "FROM medidor_oxigenacao";
 
     public boolean save(Medidor_oxigenacao medidor_oxigenacao);
+      public Medidor_oxigenacao findById(Long idMedidor_oxigenacao);
+
+    public List<Medidor_oxigenacao> findAll(); 
 }
