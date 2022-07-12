4a8f8181f596bada9af11611b7f16f8f562181d7
Vinícius Brunheroto <75040937+ViniciusBrunheroto@users.noreply.github.com>
2022-07-12 20:55:18 -0300

    Update TanqueDAO.java

--- projeto/src/main/java/br/unesp/projeto/dao/TanqueDAO.java
+++ projeto/src/main/java/br/unesp/projeto/dao/TanqueDAO.java
@@ -5,10 +5,23 @@
  */
 package br.unesp.projeto.dao;
 import br.unesp.projeto.model.Tanque;
+import java.util.List;
 
 public interface TanqueDAO {
-    final String INSERT_TANQUE = "INSERT INTO aquario(idTanque,individuos,ph,salinidade,oxigenacao,horario_alimento,filtro,QR_CODE,termostato,medidor_ph,medidor_salinidade,medidor_oxigenacao) "
+    final String INSERT_TANQUE = "INSERT INTO tanque(individuos,ph,salinidade,oxigenacao,horario_alimento,filtro,QR_CODE,termostato,medidor_ph,medidor_salinidade,medidor_oxigenacao) "
             + "VALUES(?,?,?,?,?,?,?,?,?,?,?,?)";
+    
+     final String FIND_BY_ID = "SELECT idTanque,individuos,ph,salinidade,oxigenacao,horario_alimento,filtro,QR_CODE,termostato,medidor_ph,medidor_salinidade,medidor_oxigenacao"
+            + "FROM tanque WHERE idTanque = ?";
+
+    final String FIND_ALL = "SELECT idTanque, individuos,ph,salinidade,oxigenacao,horario_alimento,filtro,QR_CODE,termostato,medidor_ph,medidor_salinidade,medidor_oxigenacao "
+            + "FROM tanque";
+     
 
     public boolean save(Tanque tanque);
+    
+    public Tanque findById(Long idTanque);
+
+    public List<Tanque> findAll(); 
+    
 }
