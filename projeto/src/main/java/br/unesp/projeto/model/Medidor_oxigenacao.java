51f212fa68306f2ae5ddbc67ef95b1858f66a169
Vinícius Brunheroto <75040937+ViniciusBrunheroto@users.noreply.github.com>
2022-07-12 11:18:54 -0300

    Update Medidor_oxigenacao.java

--- projeto/src/main/java/br/unesp/projeto/model/Medidor_oxigenacao.java
+++ projeto/src/main/java/br/unesp/projeto/model/Medidor_oxigenacao.java
@@ -25,42 +25,4 @@ public class Medidor_oxigenacao {
     public Medidor_oxigenacao () {
        // this.itemTermostato   = new ArrayList<>();
     }
-    
-    public int getoxigenacao() {
-        return oxigenacao;
-    }
-
-    public void setoxigenacao(int oxigenacao) {
-        this.oxigenacao = oxigenacao;
-    }
-    
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 41 * hash + (int) ((this.oxigenacao >>> 32) ^ this.oxigenacao);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Medidor_oxigenacao other = (Medidor_oxigenacao) obj;
-        if (this.oxigenacao != other.oxigenacao) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "Medidor_oxigenacao{" + "oxigenacao=" + oxigenacao + '}';
-    }
 }
