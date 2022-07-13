40292db633540d1059174c245fa416901ccfc341
Vinícius Brunheroto <75040937+ViniciusBrunheroto@users.noreply.github.com>
2022-07-13 19:46:04 -0300

    Update Medidor_ph.java

--- projeto/src/main/java/br/unesp/projeto/model/Medidor_ph.java
+++ projeto/src/main/java/br/unesp/projeto/model/Medidor_ph.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package br.unesp.projeto.model;
+package br.unesp.projeto.entity;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,7 +30,7 @@ public class Medidor_ph implements Serializable {
     private float ph;
     
     public Medidor_ph () {
-       // this.itemTermostato   = new ArrayList<>();
+      
     }
 
    
