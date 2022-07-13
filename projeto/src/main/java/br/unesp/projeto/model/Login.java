19359443eb17c9d7be969d7dd9abea6b4ac9e8ee
Vinícius Brunheroto <75040937+ViniciusBrunheroto@users.noreply.github.com>
2022-07-13 19:45:40 -0300

    Update Login.java

--- projeto/src/main/java/br/unesp/projeto/model/Login.java
+++ projeto/src/main/java/br/unesp/projeto/model/Login.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package br.unesp.projeto.model;
+package br.unesp.projeto.entity;
 import java.util.ArrayList;
 import java.util.List;
 
