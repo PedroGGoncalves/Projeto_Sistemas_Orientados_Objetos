0f698f3d79d013bb992a365f372bc9bd28664858
Vinícius Brunheroto <75040937+ViniciusBrunheroto@users.noreply.github.com>
2022-07-13 17:46:29 -0300

    Update Medidor_salinidade.java

--- projeto/src/main/java/br/unesp/projeto/model/Medidor_salinidade.java
+++ projeto/src/main/java/br/unesp/projeto/model/Medidor_salinidade.java
@@ -22,6 +22,7 @@ import java.io.Serializable;
 @ToString
 public class Medidor_salinidade implements Serializable {
    
+   private static final long serialVersionUID = 1L;
         
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
