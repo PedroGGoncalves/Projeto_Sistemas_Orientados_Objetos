31699352ffc152fedc75b4e50040f5b3b4ce5859
Vinícius Brunheroto <75040937+ViniciusBrunheroto@users.noreply.github.com>
2022-07-12 21:38:19 -0300

    Update ArmazemDAOImpl.java

--- projeto/src/main/java/br/unesp/projeto/dao/ArmazemDAOImpl.java
+++ projeto/src/main/java/br/unesp/projeto/dao/ArmazemDAOImpl.java
@@ -12,10 +12,13 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ArmazemDAOImpl implements ArmazemDAO {
      ArmazemDAOImpl() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+          
     }
     @Override
     public boolean save(Armazem armazem) {
@@ -30,8 +33,7 @@ public class ArmazemDAOImpl implements ArmazemDAO {
         if (con != null) {
             try {
                 pstm = con.prepareStatement(INSERT_ARMAZEM);
-                pstm.setLong(1,armazem.getIdArmazem());
-                pstm.setInt(2, armazem.getQuantidade());
+                pstm.setInt(1, armazem.getQuantidade());
 
                 pstm.executeUpdate();
 
@@ -43,4 +45,67 @@ public class ArmazemDAOImpl implements ArmazemDAO {
 
         return b;
     }
+     
+     
+     
+    @Override
+    public Armazem findById(Long idArmazem) {
+
+        Connection con = null;
+        PreparedStatement pstm = null;
+        ResultSet res = null;
+        Armazem armazem = null;
+
+        con = FabricaConexao.getConexao();
+
+        if (con != null) {
+            try {
+                pstm = con.prepareStatement(FIND_BY_ID);
+                pstm.setLong(1, IdArmazem);
+                res = pstm.executeQuery();
+
+                while (res.next()) {
+                    armazem = new Armazem();
+                    armazem.setIdArmazem(res.getLong(1));
+                    armazem.setQuantidade(res.getInt(2));
+                
+                }
+            } catch (SQLException ex) {
+                System.out.println("Message: " + ex);
+            }
+        }
+
+        return armazem;
+    } 
+     
+    @Override
+    public List<Armazem> findAll() {
+
+        Connection con = null;
+        PreparedStatement pstm = null;
+        ResultSet res = null;
+        List<Armazem> lista = new ArrayList<>();
+
+        con = FabricaConexao.getConexao();
+
+        if (con != null) {
+            try {
+                pstm = con.prepareStatement(FIND_ALL);                
+                res = pstm.executeQuery();
+                                
+                while (res.next()) {                    
+                    Armazem armazem = new Armazem();
+                    armazem.setIdArmazem(res.getLong(1));
+                    armazem.setQuantidade(res.getInt(2));
+                    
+                    lista.add(armazem);
+                }
+            } catch (SQLException ex) {
+                System.out.println("Message: " + ex);
+            }
+        }
+
+        return armazem;
+    } 
+     
 }
