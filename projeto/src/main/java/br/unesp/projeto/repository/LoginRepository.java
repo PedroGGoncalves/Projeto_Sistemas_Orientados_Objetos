807d3b709f3fe7ee49b1d7e0b5f5b588a35f1d9e
Vinícius Brunheroto <75040937+ViniciusBrunheroto@users.noreply.github.com>
2022-07-15 09:38:05 -0300

    Update LoginRepository.java

--- projeto/src/main/java/br/unesp/projeto/repository/LoginRepository.java
+++ projeto/src/main/java/br/unesp/projeto/repository/LoginRepository.java
@@ -10,5 +10,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LoginRepository extends JpaRepository<Login, Long> {
 
-    Login findByID(Long Id);
+    Login findByID(String ID);
 }
