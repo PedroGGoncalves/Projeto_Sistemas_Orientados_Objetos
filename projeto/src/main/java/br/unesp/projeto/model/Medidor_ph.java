2581c8108ef23f37041f4da01d6c00a43c5faadf
Vinícius Brunheroto <vinicius.brunheroto@unesp.br>
2022-07-15 17:49:55 -0300

    Update Medidor_ph.java

--- projeto/src/main/java/br/unesp/projeto/model/Medidor_ph.java
+++ projeto/src/main/java/br/unesp/projeto/model/Medidor_ph.java
@@ -14,6 +14,7 @@ import javax.persistence.Id;
 import java.io.Serializable;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.OneToOne;
 
 @Getter
 @Setter
@@ -28,6 +29,10 @@ public class Medidor_ph implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long idMedidor_ph;
     private float ph;
+    
+    
+    @OneToOne
+    private Tanque tanque;
     
     public Medidor_ph () {
       
