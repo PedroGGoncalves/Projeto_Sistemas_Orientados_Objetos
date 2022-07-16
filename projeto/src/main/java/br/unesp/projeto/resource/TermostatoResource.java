0f80e11b9a84106400fa2a85f228a035d393bf87
Vinícius Brunheroto <vinicius.brunheroto@unesp.br>
2022-07-16 13:17:52 -0300

    Update TermostatoResource.java

--- projeto/src/main/java/br/unesp/projeto/resource/TermostatoResource.java
+++ projeto/src/main/java/br/unesp/projeto/resource/TermostatoResource.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins="http://localhost:3000")
+@CrossOrigin(origins="http://localhost:8080")
 @RequestMapping("/termostato")
 @Slf4j
 public class TermostatoResource {
