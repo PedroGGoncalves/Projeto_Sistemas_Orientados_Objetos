21ad2999cff4d80561f6249b94e383f0bcb5df50
Vinícius Brunheroto <vinicius.brunheroto@unesp.br>
2022-07-16 13:16:54 -0300

    Update Medidor_OxigenaçãoResource.java

--- projeto/src/main/java/br/unesp/projeto/resource/Medidor_OxigenaçãoResource.java
+++ projeto/src/main/java/br/unesp/projeto/resource/Medidor_OxigenaçãoResource.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins="http://localhost:3000")
+@CrossOrigin(origins="http://localhost:8080")
 @RequestMapping("/medidor_oxigenacao")
 @Slf4j
 public class Medidor_OxigenaçãoResource {
