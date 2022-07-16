4cbc5941bb53457d4b8c9cda69ef49090b94d8ce
Vinícius Brunheroto <vinicius.brunheroto@unesp.br>
2022-07-16 13:16:22 -0300

    Update FuncionarioResource.java

--- projeto/src/main/java/br/unesp/projeto/resource/FuncionarioResource.java
+++ projeto/src/main/java/br/unesp/projeto/resource/FuncionarioResource.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins="http://localhost:3000")
+@CrossOrigin(origins="http://localhost:8080")
 @RequestMapping("/funcionario")
 @Slf4j
 public class FuncionarioResource {
