a4c95c55fe458f9df5999eb8562dbb0d80b93d73
Vinícius Brunheroto <vinicius.brunheroto@unesp.br>
2022-07-16 13:16:39 -0300

    Update LoginResource.java

--- projeto/src/main/java/br/unesp/projeto/resource/LoginResource.java
+++ projeto/src/main/java/br/unesp/projeto/resource/LoginResource.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins="http://localhost:3000")
+@CrossOrigin(origins="http://localhost:8080")
 @RequestMapping("/login")
 @Slf4j
 public class LoginResource {
